345be7b60d040f9d2b11983c96241e00dbedad71
Stewart ilondanga <stilondanga@gmail.com>
2020-06-21 15:28:19 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+var setActiveOnMouseDown = function(e) {
+  this.classList.add('active');
+}
+
+var removeActiveOnMouseUp = function(e) {
+  var labels = document.getElementsByTagName('label');
+
+  for (var i = 0; i < labels.length; i++) {
+    labels[i].classList.remove('active');
+  }
+}
+
+window.onload = function() {
+  var labels = document.getElementsByTagName('label');
+
+  for (var i = 0; i < labels.length; i++) {
+    labels[i].onmousedown = setActiveOnMouseDown;
+  }
+
+  document.body.onmouseup = removeActiveOnMouseUp;
+}
